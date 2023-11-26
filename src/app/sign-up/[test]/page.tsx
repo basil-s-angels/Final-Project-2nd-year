f662f3c0b9d7db3935b07628e0712782b5ae6601
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-11-26 19:00:21 +0800

    created auth handlers (not done), and sign up UI

--- src/app/sign-up/[test]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page({ params }: { params: { test: string } }) {
-  return <div>My Post: {params.test}</div>;
-}
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,55 @@
+import { Button } from "@/components/ui/button";
+
 export default function signUp() {
   return (
-    <main>
-      <h1>watever</h1>
+    <main className="h-[100vh]">
+      <h1 className="ml-10 mt-4 mb-4 text-lg font-bold">Sign Up as an Admin</h1>
+      <form className="flex flex-col gap-2 mx-auto max-w-md">
+        <label htmlFor="position" className="-mt-3 text-center">
+          Select user position:
+        </label>
+        <select
+          id="position"
+          name="position"
+          className="pl-3 w-40 text-center mb-3 self-center border border-black"
+          required
+        >
+          <option value="admin">Admin</option>
+          <option value="cook">Cook</option>
+          <option value="waiter">Waiter</option>
+        </select>
+        <input
+          type="text"
+          placeholder="First Name"
+          className="pl-2 border border-black"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          className="pl-2 border border-black"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          className="pl-2 border border-black"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="pl-2 border border-black"
+          required
+        />
+        <Button
+          variant={"default"}
+          type="submit"
+          className="w-40 self-center bg-green-800 mt-4"
+        >
+          Submit
+        </Button>
+      </form>
     </main>
   );
 }
