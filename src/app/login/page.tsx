ba0442fa077e0822ec4fad83f17d7ae70c8b1b96
basil amso <124886339+HiItsBasilio@users.noreply.github.com>
2023-11-28 11:31:06 +0800

    semi final output for the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,40 +1,67 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import * as React from "react";
+import { Button } from "@/components/ui/button";
 
+export default function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-const LoginPage: React.FC = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
 
-    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
-    };
+    const response = await fetch("/auth/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
+    if (response.ok) {
+      const { token } = await response.json();
+      console.log("Received token:", token);
+      // Save the token and redirect the user
+    } else {
+      const { message } = await response.json();
+      console.error("Login failed:", message);
+      // Show an error message to the user
+    }
+  };
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        // Perform login logic here
-        console.log(`Username: ${username}, Password: ${password}`);
-    };
-
-    return (
-        <div className="login-page">
-            <form onSubmit={handleSubmit} className="login-form">
-                <label>
-                    Username:
-                    <input type="text" value={username} onChange={handleUsernameChange} />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={handlePasswordChange} />
-                </label>
-                <input type="submit" value="Log In" />
-            </form>
-        </div>
-    );
-};
-
-export default LoginPage;
+  return (
+    <main className="h-[100vh] flex flex-col justify-center items-center">
+      <div className="border border-white p-6 text-center text-black">
+        <h1 className="text-lg font-bold mb-6 text-white">Login</h1>
+        <p className="mb-3 text-white">Welcome back, see you</p>
+        <form
+          className="flex flex-col gap-3 mx-auto max-w-md"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            placeholder="Email"
+            className="pl-2 border border-black text-black"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="pl-2 border border-black text-black"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            variant={"default"}
+            type="submit"
+            className="w-40 self-center bg-green-800 mt-4"
+          >
+            Confirm
+          </Button>
+        </form>
+      </div>
+    </main>
+  );
+}
--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,138 +1,82 @@
-// import bcrypt from 'bcrypt';
-// import bodyParser from 'body-parser';
-// import express from 'express';
-// import jwt from 'jsonwebtoken';
-// import { Pool } from 'pg';
+// Import necessary modules
+import bcrypt from "bcrypt";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { Pool } from "pg";
 
-// const pool = new Pool({
-//     user: 'your_pg_user',
-//     host: 'localhost',
-//     database: 'your_db_name',
-//     password: 'your_db_password',
-//     port: 5432,
-// });
+interface User {
+  id: number;
+  email: string;
+  password: string;
+}
 
+const pool = new Pool({
+  user: "your_pg_user",
+  host: "localhost",
+  database: "your_db_name",
+  password: "your_db_password",
+  port: 5432,
+});
 
-// async function startServer() {
-//     // Create an Express app
-//     const app = express();
+// Define the loginUser function which handles the login logic
+async function loginUser(email: string, password: string): Promise<string> {
+  // Query the database for a user with the provided email
+  const { rows } = await pool.query<User>(
+    "SELECT * FROM users WHERE email = $1",
+    [email],
+  );
 
-//     // Connect to the PostgreSQL database using a connection pool
-//     const connection = await pool.connect();
+  // If no user is found, throw an error
+  if (rows.length === 0) {
+    throw new Error("Email or password is incorrect");
+  }
 
-//     // Middleware for parsing JSON and URL-encoded data
-//     app.use(bodyParser.json());
-//     app.use(bodyParser.urlencoded({ extended: true }));
+  // Compare the provided password with the hashed password in the database
+  const correctPassword = await bcrypt.compare(password, rows[0].password);
 
-//     // Endpoint for user registration
-//     app.post('/register', async (request, response) => {
-//         const { email, password } = request.body;
-//         const hashedPassword = await bcrypt.hash(password, 10);
+  // If the passwords do not match, throw an error
+  if (!correctPassword) {
+    throw new Error("Email or password is incorrect");
+  }
 
-//         // Insert a new user into the 'users' table and return the id and email of the created user
-//         const { rows } = await connection.query(
-//             'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email',
-//             [email, hashedPassword],
-//         );
+  // If the passwords match, create a JWT token for the user
+  const token = jwt.sign(
+    { userId: rows[0].id, email: rows[0].email },
+    "secret", // Replace with a more secure secret
+    {
+      expiresIn: "1h",
+    },
+  );
 
-//         response.json({ createdUser: rows[0] });
-//     });
+  // Return the token
+  return token;
+}
 
-//     // Endpoint for user login
-//     app.post('/login', async (request, response) => {
-//         const { email, password } = request.body;
+// Define the startServer function which starts the Express server
+async function startServer() {
+  // Create a new Express app
+  const app = express();
 
-//         // Retrieve user information based on the provided email
-//         const { rows } = await connection.query(
-//             'SELECT * FROM users WHERE email = $1',
-//             [email],
-//         );
+  // Define the /login endpoint
+  app.post("/login", async (request: Request, response: Response) => {
+    try {
+      // Extract email and password from the request body
+      const { email, password } = request.body;
 
-//         if (rows.length === 0) {
-//             // If no user with the provided email is found, return an error
-//             response.status(401).json({ message: 'Email or password is incorrect' });
-//         } else {
-//             // Check if the provided password matches the hashed password stored in the database
-//             const correctPassword = await bcrypt.compare(password, rows[0].password);
+      // Call the loginUser function and get the token
+      const token = await loginUser(email, password);
 
-//             if (correctPassword) {
-//                 // If the password is correct, create a JWT token and send it as a response
-//                 const token = jwt.sign(
-//                     { userId: rows[0].id, email: rows[0].email },
-//                     'secret', // Replace with a more secure secret
-//                     {
-//                         expiresIn: '1h',
-//                     },
-//                 );
-//                 response.json({ token });
-//             } else {
-//                 // If the password is incorrect, return an error
-//                 response.status(401).json({ message: 'Email or password is incorrect' });
-//             }
-//         }
-//     });
+      // Send the token in the response
+      response.json({ token });
+    } catch (error) {
+      // If an error occurs, send an error message in the response
+      if (error instanceof Error) {
+        response.status(401).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: "An unexpected error occurred" });
+      }
+    }
+  });
+}
 
-//     // Endpoint for getting user information
-//     app.get('/me', async (request, response) => {
-//         const authHeader = request.header('Authorization');
-//         const token = authHeader?.split(' ')[1];
-
-//         if (!token) {
-//             // If no token is provided, return an authentication error
-//             response.status(401).json({ message: 'Not authenticated' });
-//             return;
-//         }
-
-//         try {
-//             // Verify the JWT token and get user claims
-//             const claims = jwt.verify(token, 'secret'); // Replace with the same secret used for signing
-//             const { userId } = claims as any;
-
-//             // Retrieve user information from the database based on the user id
-//             const { rows } = await connection.query(
-//                 'SELECT id, email, presidentialBet FROM users WHERE id = $1',
-//                 [userId],
-//             );
-//             response.json({ me: rows[0] });
-//         } catch (error) {
-//             // If there is an error in token verification, return an authentication error
-//             response.status(401).json({ message: 'Not authenticated' });
-//         }
-//     });
-
-//     // Endpoint for fetching channels
-//     app.get('/channels', async (request, response) => {
-//         // Retrieve all channels from the 'channels' table
-//         const result = await connection.query(
-//             'SELECT * FROM channels ORDER BY name',
-//         );
-//         response.json(result.rows);
-//     });
-
-//     // Endpoint for fetching messages for a specific channel
-//     app.get('/messages/:channelName', async (request, response) => {
-//         // Retrieve messages for a specific channel by joining the 'messages' and 'channels' tables
-//         const result = await connection.query(
-//             /* sql */
-//             `
-//             SELECT messages.* FROM messages
-//             INNER JOIN channels ON messages.channel_id = channels.id
-//             WHERE name = $1
-//           `,
-//             [request.params.channelName],
-//         );
-//         console.log('DB results', result.rows);
-//         response.json(result.rows);
-//     });
-
-//     // Serve static files from the 'public' directory
-//     app.use(express.static('public'));
-
-//     // Start the server on port 3000
-//     app.listen(3000, () => {
-//         console.log('Server has started at http://localhost:3000');
-//     });
-// }
-
-// // Call the function to start the server
-// startServer();
+startServer();
