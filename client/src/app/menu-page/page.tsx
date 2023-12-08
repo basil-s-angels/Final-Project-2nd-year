16029b246c7a6cfdd2f09165025a2c87ddcc524a
Raine Christine Perez <125470134+raynieee@users.noreply.github.com>
2023-12-09 02:10:11 +0800

    modified menu page interface

--- client/src/app/menu-page/page.tsx
+++ client/src/app/menu-page/page.tsx
@@ -1,41 +1,95 @@
 "use client"
-import React from 'react';
-import Image from 'next/image';
+import React, { useState } from 'react';
 
-const FoodMenuPage = () => {
-  const foodItems = [
-    { name: 'Margherita Pizza', description: 'Classic cheese and tomato base', price: '$8.99', imageUrl: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D' },
-    { name: 'Caesar Salad', description: 'Romaine lettuce with Caesar dressing', price: '$5.99', imageUrl: '' },
-    { name: 'Spaghetti Carbonara', description: 'Pasta with creamy bacon sauce', price: '$10.99', imageUrl: '' },
-    { name: 'Grilled Salmon', description: 'Fresh salmon with a lemon butter glaze', price: '$15.99', imageUrl: '' },
-    { name: 'Chocolate Lava Cake', description: 'Warm cake with a gooey chocolate center', price: '$6.99', imageUrl: '' },
+interface FoodItem {
+  name: string;
+  description: string;
+  price: string;
+  imageUrl: string;
+  type: string;
+}
+
+const FoodMenuPage: React.FC = () => {
+  const foodItems: FoodItem[] = [
+    { name: 'Caesar Salad', description: 'Romaine lettuce with Caesar dressing', price: '$5.99', imageUrl: 'https://as2.ftcdn.net/v2/jpg/02/02/48/35/1000_F_202483549_3cDh8uaQ5OJG9GUDsp9YKSQNt69rjucc.jpg', type: 'appetizer' },
+    { name: 'Spaghetti Carbonara', description: 'Pasta with creamy bacon sauce', price: '$10.99', imageUrl: 'https://as1.ftcdn.net/v2/jpg/06/00/16/22/1000_F_600162257_rnJb3XPSr3arrHECJlb47cnMrNvA76Ys.jpg', type: 'main' },
+    { name: 'Grilled Salmon', description: 'Fresh salmon with a lemon butter glaze', price: '$15.99', imageUrl: 'https://as1.ftcdn.net/v2/jpg/03/25/35/08/1000_F_325350805_D8PVU73qs1dj5TdWgm9IpuAjJ7sgHacK.jpg', type: 'main' },
+    { name: 'Chocolate Lava Cake', description: 'Warm cake with a gooey chocolate center', price: '$6.99', imageUrl: 'https://as1.ftcdn.net/v2/jpg/06/45/68/86/1000_F_645688685_jeEJGhKe4TQWZuPEyPVpcga5u9WFRpga.jpg', type: 'dessert' },
+    { name: 'Margherita Pizza', description: 'Classic cheese and tomato base', price: '$8.99', imageUrl: 'https://t4.ftcdn.net/jpg/02/16/71/25/240_F_216712544_4XA06NXDDg9bBWrtCQ1I2NYn8cIrnZ2h.jpg', type: 'pizza' },
+    { name: 'Pepperoni Pizza', description: 'Mozzarella cheese, salami, pepper, tomatoes, spices and fresh basil', price: '9.99', imageUrl: 'https://as1.ftcdn.net/v2/jpg/02/16/78/88/1000_F_216788898_nP9aPoisuMzj3IvSKGxJAyCRUiSX1PGe.jpg', type: 'pizza' }
   ];
 
-  const handleAddToBasket = (itemName: string) => {
-    // Implement the logic to add the item to the basket
-    console.log(`${itemName} added to basket!`);
+  const [quantities, setQuantities] = useState<Array<number>>(Array(foodItems.length).fill(1));
+
+  const addQuantity = (index: number) => {
+    setQuantities((prevQuantities) => {
+      const newQuantities = [...prevQuantities];
+      newQuantities[index] += 1;
+      return newQuantities;
+    });
   };
+
+  const minusQuantity = (index: number) => {
+    if (quantities[index] > 1) {
+      setQuantities((prevQuantities) => {
+        const newQuantities = [...prevQuantities];
+        newQuantities[index] -= 1;
+        return newQuantities;
+      });
+    }
+  };
+
+  const handleAddToBasket = (itemName: string, quantity: number) => {
+    // Implement the logic to add the item to the basket
+    console.log(`${itemName} added to basket with quantity: ${quantity}`);
+  };
+
+  // Group food items by type
+  const groupedFoodItems: Record<string, FoodItem[]> = foodItems.reduce((result: any, item) => {
+    if (!result[item.type]) {
+      result[item.type] = [];
+    }
+    result[item.type].push(item);
+    return result;
+  }, {});
 
   return (
     <div className='w-[80%] m-auto text-center'>
-      <h1>Our Menu</h1>
-      <div className='grid grid-cols-3 gap-[10px] p-[10px]'>
-        {foodItems.map((item, index) => (
-          <div key={index} className='border-[1px] border-[#ccc] text-left h-[300px] flex flex-col justify-between p-2'>
-            <div className='w-full h-[150px] relative'>
-              <img src={item.imageUrl} alt={item.name} className='mx-auto object-cover h-full w-full'/>
-            </div>
-            <h2>{item.name}</h2>
-            <p>{item.description}</p>
-            <p className='font-bold text-white'>{item.price}</p>
-            <button onClick={() => handleAddToBasket(item.name)} className='bg-green-700 py-[10px] px-[20px] rounded-[5px] cursor-pointer'>
-              Add to Basket
-            </button>
+      <h1 className='xl:text-5xl text-4xl mt-2'>MENU</h1>
+      {Object.entries(groupedFoodItems).map(([category, items]) => (
+        <div key={category} className='mb-4 mt-4'>
+          <h2 className='text-2xl font-semibold capitalize text-slate-300'>{category}</h2>
+          <hr className='m-3'></hr>
+          <div className='grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-[10px] p-[10px] mb-3'>
+            {items.map((item, index) => (
+              <div key={index} className='border-[1px] border-[#ccc] rounded-lg text-left h-[340px] flex flex-col justify-between p-2'>
+                <div className='w-full h-[150px] relative'>
+                  <img src={item.imageUrl} alt={item.name} className='mx-auto object-cover h-full w-full'/>
+                </div>
+                  <h2 className='font-semibold'>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <div className='grid grid-cols-2'>
+                    <p className='font-bold text-white'>{item.price}</p>
+                    <div className='grid grid-cols-2 space-x-2'>
+                      <p>Quantity</p>
+                      <div className='grid grid-cols-3 bg-slate-200 text-black rounded-sm'>
+                        <button onClick={() => minusQuantity(index)}>-</button>
+                        <p className='text-center'>{quantities[index]}</p>
+                        <button onClick={() => addQuantity(index)}>+</button>
+                      </div>
+                    </div>
+                  </div>
+                <button onClick={() => handleAddToBasket(item.name, quantities[index])} className='bg-green-700 py-[10px] px-[20px] rounded-[5px] cursor-pointer font-medium'>
+                  Add to Basket
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+      <button className='font-bold xl:w-[100%] w-[95%] mt-3 py-3 cursor-pointer bg-green-600 rounded-sm xl:text-xl'>Basket</button>
     </div>
   );
 };
 
-export default FoodMenuPage;
+export default FoodMenuPage
