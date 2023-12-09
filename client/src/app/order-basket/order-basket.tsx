711767512976b78841d5b06f727dd977c5c85c3e
Thel-nny <thelanny.maguillano-22@cpu.edu.ph>
2023-12-09 16:36:03 +0800

    I changed some lines in Order-tsx. I made it more responsive.

--- client/src/app/order-basket/order-basket.tsx
+++ client/src/app/order-basket/order-basket.tsx
@@ -40,26 +40,24 @@ const Ordercart: React.FC = () => {
     const calculateSubtotal = () => {
         return cartItems.reduce((total, item, index) => total + item.price * quantities[index], 0);
       };
-    
-
+      
     const handleCheckout = () => {
         alert('Thank you for choosing us!')
     }
     return (
-        
             <div className=" h-screen py-8">
                 <div className="container mx-auto px-4">
-                    <h1 className="text-2xl text-white-500 font-semibold mb-4"> ORDER SUMMARY</h1>
+                    <h1 className=" md:text-2xl sm:text-sm text-white-500 font-semibold mb-4"> ORDER SUMMARY</h1>
                     <div className="flex flex-col md:flex-row gap-4 pt-8">
                         <div className="md:w-3/4">
                             <div className="bg-white rounded-lg shadow-md p-6 mb-4">
                                 <table className="w-full">
                                     <thead>
                                         <tr>
-                                            <th className="text-left text-xl text-blue-600 font-semibold"> Food Name</th>
-                                            <th className="text-left text-xl text-blue-600 font-semibold"> Price</th>
-                                            <th className="text-left text-xl text-blue-600 font-semibold pl-6"> Quantity</th>
-                                            <th className="text-left text-xl text-blue-600 font-semibold"> total</th>
+                                            <th className="md:text-xl sm:text-sm text-left  text-blue-600 font-semibold"> Food Name</th>
+                                            <th className="md:text-xl sm:text-sm text-left text-blue-600 font-semibold"> Price</th>
+                                            <th className="md:text-xl sm:text-sm text-left text-blue-600 font-semibold ml-6"> Quantity</th>
+                                            <th className="md:text-xl sm:text-sm text-left text-blue-600 font-semibold"> Total</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -67,19 +65,19 @@ const Ordercart: React.FC = () => {
                                         <tr key={item.id}>
                                             <td className="py-4">
                                                 <div className="flex-items-center">
-                                                    <img className ="h-20 w-18 mr-4" src={item.imageurl} alt="Food image 1" />
+                                                    <img className ="h-20 w-18 mr-4"  src={item.imageurl} alt="Food image 1" />
                                                     <span className=" text-blue-800 font-semibold pl-4"> {item.name} </span>
                                                 </div>
                                             </td>
-                                            <td className="py-4 text-gray-700"> {item.price.toFixed(2)}</td>
+                                            <td className="xl:text-lg text-sm py-4 text-gray-700"> {item.price.toFixed(2)}</td>
                                             <td className="py-4">
                                                 <div className="flex-items-center">
-                                                    <button className="border rounded-md py-2 px-4 mr-2 text-gray-700" onClick={() => minusQuantity(index)}>-</button>
+                                                    <button className="md:border rounded-md md:py-2 md:px-4 md:mr-2 ml-2 text-gray-700 " onClick={() => minusQuantity(index)}>-</button>
                                                     <span className="text-center text-gray-700 w-9 mx-2">{quantities[index]}</span>
-                                                    <button className="border rounded-md py-2 px-4 ml-2 text-gray-700" onClick={() => addQuantity(index)}>+</button>
+                                                    <button className="md:border rounded-md md:py-2 md:px-4 md:ml-2 text-gray-700" onClick={() => addQuantity(index)}>+</button>
                                                 </div>
                                             </td>
-                                            <td className=" text-gray-800 py-4">{item.price * quantities[index]}</td>
+                                            <td className="md:text-xl text-sm text-gray-800 py-4">{(item.price * quantities[index]).toFixed(2)}</td>
                                         </tr>
                                         ))}
                                     </tbody>
@@ -87,10 +85,8 @@ const Ordercart: React.FC = () => {
                             </div>
                         </div>
                         <div className="md:w-1/4">
-                        
                             <div className="bg-white rounded-lg shadow-md p-6">
                                 <h2 className="text-lg text-gray-600 font-semibold mb-4"> Summary</h2>
-                                
                                 <div className="flex justify-between mb-2">
                                     <span className="text-blue-600 text-xl">Subtotal</span>
                                     <span className="text-gray-600">{calculateSubtotal().toFixed(2)}</span>
@@ -101,16 +97,10 @@ const Ordercart: React.FC = () => {
                                 </div>
                                 <button className="bg-blue-500 text-white-700 py-2 px-4 rounded-lg mt-4 w-full" onClick={() => handleCheckout}> Place Order</button>
                             </div>
-                            
-                        </div>
-                        
+                        </div> 
                     </div>
-
                 </div>
-    
-
             </div>
     )
 }
-
 export default Ordercart
