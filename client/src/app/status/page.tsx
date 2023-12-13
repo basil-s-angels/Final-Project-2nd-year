c9b976e4dd6c48d951cc72b074270b04cb757e1e
Shawn Timothy Ike Barza <92981591+Pistachio21@users.noreply.github.com>
2023-12-13 23:39:25 +0800

    updated css

--- client/src/app/status/page.tsx
+++ client/src/app/status/page.tsx
@@ -11,12 +11,18 @@ const Status = () => {
           <div className="flex items-center justify-center">
             <p className="text-lg font-bold">
               <div className='grid grid-cols-2'>
-                <p className='text-left'>Fried Chicken</p><p className='text-right'>Quantity: 2</p>
-                <p className='text-left'>Spaghetti</p><p className='text-right'>Quantity: 4</p>
+                <p className='text-left text-lg'>Fried Chicken</p><p className='text-right'>Quantity: 2</p>
+                <p className='text-left text-lg'>Spaghetti</p><p className='text-right'>Quantity: 4</p>
               </div>
               <div className="border-t border-gray-300 mb-2"></div>
               <div className='grid grid-cols-2'>
               </div>
+              <div className='p-10'>
+              The status of your order: <strong className='text-blue-400 text-lg'>PENDING</strong><br></br>
+              The status of your order: <strong className='text-yellow-400 text-lg'>PREPARING</strong><br></br>
+              The status of your order: <strong className='text-pink-400 text-lg'>SERVING</strong><br></br>
+              The status of your order: <strong className='text-green-400 text-lg'>COMPLETED</strong>
+            </div>
             </p><br></br>
           </div>
         </div>
