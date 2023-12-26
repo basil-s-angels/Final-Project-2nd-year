baf97820c4edccfa9128e98acc9d45d5e6cc9d4b
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-12-26 15:34:16 +0800

    Revert "more deployment testing"
    This reverts commit 7a23ef08bcf81072fc15c047cddeda8b3f2211b6.

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -13,6 +13,9 @@ export async function middleware(request: NextRequest) {
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        token: request.cookies.get("token"),
+      }),
     },
   );
 
