e6e34762de15c9fe737c5dc21645327966c518da
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2024-01-06 13:33:17 +0800

    comment out middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,63 +1,63 @@
-/* eslint-disable no-unused-vars */
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// /* eslint-disable no-unused-vars */
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  // console.log(request, "from middleware")
-  try {
-    const pathname = request.nextUrl.pathname;
+// export async function middleware(request: NextRequest) {
+//   // console.log(request, "from middleware")
+//   try {
+//     const pathname = request.nextUrl.pathname;
 
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/user-middleware`,
-      {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          token: request.cookies.get("token"),
-        }),
-      },
-    );
+//     const response = await fetch(
+//       `${process.env.NEXT_PUBLIC_SERVER_URL}/user-middleware`,
+//       {
+//         method: "POST",
+//         credentials: "include",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           token: request.cookies.get("token"),
+//         }),
+//       },
+//     );
 
-    if (response.ok) {
-      const user = await response.json();
-      console.log(user.decoded, "gotten from middleware");
-      if (user.decoded.position === "admin") {
-        if (pathname === "/admin/login" || pathname === "/admin/signup") {
-          return NextResponse.redirect(
-            `${process.env.NEXT_PUBLIC_CLIENT_URL}/admin`,
-          );
-        } else {
-          return NextResponse.next();
-        }
-      } else {
-        if (
-          pathname === "/admin" ||
-          pathname === "/admin/login" ||
-          pathname === "/admin/signup"
-        ) {
-          return NextResponse.redirect(
-            `${process.env.NEXT_PUBLIC_CLIENT_URL}/admin/employee-page`,
-          );
-        } else {
-          return NextResponse.next();
-        }
-      }
-    } else {
-      if (
-        pathname.startsWith("/admin") &&
-        pathname !== "/admin/login" &&
-        pathname !== "/admin/signup"
-      ) {
-        return NextResponse.redirect(
-          `${process.env.NEXT_PUBLIC_CLIENT_URL}/admin/login`,
-        );
-      }
-      return NextResponse.next();
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     if (response.ok) {
+//       const user = await response.json();
+//       console.log(user.decoded, "gotten from middleware");
+//       if (user.decoded.position === "admin") {
+//         if (pathname === "/admin/login" || pathname === "/admin/signup") {
+//           return NextResponse.redirect(
+//             `${process.env.NEXT_PUBLIC_CLIENT_URL}/admin`,
+//           );
+//         } else {
+//           return NextResponse.next();
+//         }
+//       } else {
+//         if (
+//           pathname === "/admin" ||
+//           pathname === "/admin/login" ||
+//           pathname === "/admin/signup"
+//         ) {
+//           return NextResponse.redirect(
+//             `${process.env.NEXT_PUBLIC_CLIENT_URL}/admin/employee-page`,
+//           );
+//         } else {
+//           return NextResponse.next();
+//         }
+//       }
+//     } else {
+//       if (
+//         pathname.startsWith("/admin") &&
+//         pathname !== "/admin/login" &&
+//         pathname !== "/admin/signup"
+//       ) {
+//         return NextResponse.redirect(
+//           `${process.env.NEXT_PUBLIC_CLIENT_URL}/admin/login`,
+//         );
+//       }
+//       return NextResponse.next();
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
